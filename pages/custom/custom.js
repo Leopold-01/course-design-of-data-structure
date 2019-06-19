8fd7f8c825d9673dae22dae562f6900fe7f06916
Rimsky <349157738@qq.com>
2019-06-19 22:41:31 +0800

    last page  front size

--- pages/custom/custom.js
+++ pages/custom/custom.js
@@ -4,9 +4,50 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    array: ['最短距离', '最低花费', '最短时间'],
+    index: 0,
+    isPicker: false,
+    reminder: "这是一个可以点的东西",
+    isShow:false,
+    Up:true,
+    start:'合肥',
+    destination: '厦门'
   },
-
+  chageToPicker:function (){
+      //只在第一次点之前显示提示
+    if(!this.data.isPicker)
+    {
+        this.setData({
+            isPicker: true
+        })
+    }
+  },
+  bindPickerChange:function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+     this.setData({
+         index: e.detail.value
+     })
+  },
+  showList:function () {
+    this.setData({
+        isShow: true,
+        Up:true
+    })
+  },
+  down: function (){
+    console.log("down")
+    this.setData({
+      
+      Up:false
+    })
+  },
+  back:function (){
+    this.setData({
+      isShow:false,
+      
+    })
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
